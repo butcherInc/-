c495d5287503352b307350ac46c760a42c1aef6f
butcherInc <843968535@qq.com>
2016-07-07 19:50:44 +0800

    改变工程结构，但是还有乱码。

--- wu_zi_qi/src/Qipant.java
+++ wu_zi_qi/src/Qipant.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.geom.*;
@@ -16,123 +15,77 @@ public class Qipant extends Frame{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	//¿ØÖÆÆåÅÌµÄ²ÎÊı
-	Paint paint1;
-	static int click=0;
-	static double a=10;static double b=100; 
+	//æ§åˆ¶æ£‹ç›˜çš„å‚æ•°
+	Paint myPaint;
+	static int click=0;  //æœ‰æ•ˆç‚¹å‡»æ¬¡æ•°  å¥‡æ•°æ¬¡ä¸ºè“æ–¹  å¶æ•°æ¬¡ä¸ºé»‘æ–¹
 	static double x;static double y;static boolean in;
-	
-    //¿ØÖÆÊı×éÒÔ¼°ÅĞ¶ÏÓï¾äµÄ²ÎÊı
-	static int[][] mapx=new int[40][40];
-	// black
-	static int num1=0;static int num2=0;
-	static int num3=0;static int num4=0;
-	// blue
-	static int count1;static int count2;
-	static int count3;static int count4;
-	static boolean[][] map1 = new boolean[40][40];
-   
-	public static void main(String[] args){
-	    Qipant ob1=new Qipant();
-	    ob1.initializemapx();
-	    
-	    Frame f=new Qipant("Îå×ÓÆå");
-	    f.setVisible(true);
-		//
+
+	Qipant(String blackOrBlue,String gameOver){
+		super(gameOver);
+		Panel p=new Panel();
+		p.setLayout(new FlowLayout());
+		p.add(new Button("yes"));
+		p.add(new Button("no"));
+		if(blackOrBlue.equals("black")){
+			setBackground(Color.LIGHT_GRAY);
+			add(new Label("the black site is the big ass.."),BorderLayout.CENTER);
+		}
+		if(blackOrBlue.equals("blue")){
+			setBackground(Color.blue);
+			add(new Label("the blue site is the boss!!"),BorderLayout.CENTER);
+			}
+		add(p,BorderLayout.SOUTH);
+		setSize(300,150);
+    	setVisible(true);
+    	WindowListener win=new Win_handler();
+		addWindowListener(win);
 	}
-	Qipant(){}
 	Qipant(String title){
 		super(title);
-		Button Button_start=new Button("¿ªÊ¼ÓÎÏ·");
-		Button Button_reset=new Button("ÖØÖÃÓÎÏ·");
+		Button Button_start=new Button("å¼€å§‹æ¸¸æˆ");
+		Button Button_reset=new Button("é‡ç½®æ¸¸æˆ");
 		setLayout(new FlowLayout());
 		add(Button_start);
 		add(Button_reset);
-		setSize(420,525);
+		setSize(420,525); 
 		setBackground(Color.magenta);
 		MenuBar mb=new MenuBar();
-		Menu game=new Menu("ÓÎÏ·");
+		Menu game=new Menu("æ¸¸æˆ");
 		mb.add(game);
-		MenuItem start=new MenuItem("¿ªÊ¼ÓÎÏ·");
-		MenuItem reset=new MenuItem("ÖØÖÃÓÎÏ·");
-		MenuItem quit=new MenuItem("½áÊøÓÎÏ·");
+		MenuItem start=new MenuItem("å¼€å§‹æ¸¸æˆ");
+		MenuItem reset=new MenuItem("é‡ç½®æ¸¸æˆ");
+		MenuItem quit=new MenuItem("ç»“æŸæ¸¸æˆ");
 		game.add(start);
 		game.add(reset);
 		game.add(quit);
 		setMenuBar(mb);
-		//
+		//æ£‹ç›˜çš„å½¢çŠ¶
 		BufferedImage buf=new BufferedImage(10,10,BufferedImage.TYPE_INT_RGB);
 		Graphics2D g2buf=(Graphics2D)buf.getGraphics();
 		g2buf.setPaint(Color.pink);
 		g2buf.fill(new Rectangle2D.Double(0,0,10,10));
 		g2buf.setPaint(Color.white);
 		g2buf.fill(new Ellipse2D.Double(0,0,10,10));
-		paint1=new TexturePaint(buf,new Rectangle(0,0,10,10));
-		//¼àÌıÀàÒÔÕâÖÖĞÎÊ½·ÅÔÚ ¹¹Ôìº¯ÊıÄÚ  ÁôÔÚÕâÀïµ±×ö±ê±¾
-		//ÔÚ¹¹ÔìÕâ¸öÀàµÄ¶ÔÏóµÄÊ±ºò¾ÍÎªÕâ¸ö´°Ìå¼ÓÈëÊó±ê¼àÌı ²¢ÉèÖÃÏàÓ¦µÄ´¦ÀíÁ÷³Ì
+		myPaint=new TexturePaint(buf,new Rectangle(0,0,10,10));
+		//ç›‘å¬ç±»ä»¥è¿™ç§å½¢å¼æ”¾åœ¨ æ„é€ å‡½æ•°å†…  ç•™åœ¨è¿™é‡Œå½“åšæ ‡æœ¬
 		addMouseListener(new MouseAdapter(){
 			public void mousePressed(MouseEvent e){
-
+				System.out.println("mouseListener: "+Thread.currentThread().getName());
 				x=e.getX(); y=e.getY();
-				click++;
+				
 			    Double rectPath=new Rectangle2D.Double(10,100,400,400);
 			    in=rectPath.contains(x,y);
-			    Qipant ob=new Qipant();
-			    ob.locate(x, y);    //¶¯Ì¬¸üĞÂa b
+			    if(in){click++;}
+			    actionProcessThread.x = x;
+			    actionProcessThread.y = y;
+			    actionProcessThread.click = click;
+			    actionProcessThread.in = in;
 			    
-			    
-				if(in && mapx[((int)a-10)/10][((int)b-100)/10]==0)
-				{
-					ob.output(x,y,(int)a,(int)b,in);
-				    if(Bw()){
-				    	mapx[((int)a-10)/10][((int)b-100)/10]=1;
-				    	num1=0;   //Ê¹num1µÈÓÚÁã  Ö´ĞĞÍêÅĞ¶ÏÓï¾ä Ö»È¡num1Ôö¼ÓµÄ²¿·Ö È»ºóÏÂÒ»´Î ÔÙÖÃÁã
-				    	ob.initializeBoolean(1, mapx);
-				    	ob.fanxiexy(((int)a-10)/10,((int)b-100)/10,1,mapx,map1);
-				
-				    	num2=0;
-				    	ob.initializeBoolean(1, mapx);
-				    	ob.zhengxiexy(((int)a-10)/10,((int)b-100)/10,1,mapx,map1);
-				    	
-				    	num3=0;
-				    	ob.initializeBoolean(1, mapx);
-				    	ob.shuipingxy(((int)a-10)/10,((int)b-100)/10,1,mapx,map1);
-				    	
-				    	num4=0;
-				    	ob.initializeBoolean(1, mapx);
-				    	ob.chuizhixy(((int)a-10)/10,((int)b-100)/10,1,mapx,map1);
-				    	System.out.println("BLACK:num1="+num1+"  num2="+num2+"  num3="+num3+"  num4="+num4);
-				    	}
-				    else{
-				    	mapx[((int)a-10)/10][((int)b-100)/10]=-1;
-				    	num1=0;
-				    	ob.initializeBoolean(-1, mapx);
-				    	ob.fanxiexy(((int)a-10)/10,((int)b-100)/10,-1,mapx,map1);
-				    	count1=num1; num1=0;
-			            
-				    	num2=0;
-				    	ob.initializeBoolean(-1, mapx);
-				    	ob.zhengxiexy(((int)a-10)/10,((int)b-100)/10,-1,mapx,map1);
-				    	count2=num2; num2=0;
-				    	
-				    	num3=0;
-				    	ob.initializeBoolean(-1, mapx);
-				    	ob.shuipingxy(((int)a-10)/10,((int)b-100)/10,-1,mapx,map1);
-				    	count3=num3; num3=0;
-				    	
-				    	num4=0;
-				    	ob.initializeBoolean(-1, mapx);
-				    	ob.chuizhixy(((int)a-10)/10,((int)b-100)/10,-1,mapx,map1);
-				    	count4=num4; num4=0;
-				    	
-				    	System.out.println("BLUE:count1="+count1+"  count2="+count2+"  count3="+count3+"  count4="+count4);
-				    	}
-		    	    repaint((int)a,(int)b,10,10);
-		    	    ob.listener(num1, num2, num3, num4, count1, count2, count3, count4);
-				}
-				else ob.output(x,y,(int)a,(int)b,in);
+			    new actionProcessThread().run();
+			    repaint((int)actionProcessThread.a,(int)actionProcessThread.b,10,10);
+			    actionProcessThread.listener(actionProcessThread.num1, actionProcessThread.num2, actionProcessThread.num3, actionProcessThread.num4, 
+			    		                     actionProcessThread.count1, actionProcessThread.count2, actionProcessThread.count3, actionProcessThread.count4);
 			}
-			
 		});
 		ActionListener al_start=new Button_start_handler();
 		Button_start.addActionListener(al_start);
@@ -151,10 +104,9 @@ public class Qipant extends Frame{
 			System.out.println("button_start");
 		}
 	}
-	class Button_reset_handler implements ActionListener{
+	class Button_reset_handler implements ActionListener{//åº”è¯¥åˆå§‹åŒ–æ‰€æœ‰çš„æ•°æ® è€Œä¸åªæ˜¯mapx[][]æ•°ç»„çš„æ•°æ®
 		public void actionPerformed(ActionEvent e){
-			Qipant ob=new Qipant();
-			ob.initializemapx();
+			actionProcessThread.initializemapx();
 			repaint();
 		}
 	}
@@ -210,10 +162,13 @@ public class Qipant extends Frame{
 	}
 	public void paint(Graphics g){
 		    Graphics2D g2=(Graphics2D)g;
+		    int[][] mapx = actionProcessThread.mapx;
+		    double a = actionProcessThread.a;
+		    double b = actionProcessThread.b;
 		    
 		    if(mapx[((int)a-10)/10][((int)b-100)/10]==0){
-		    	g2.setPaint(paint1);
-	    	    g2.fill(new Rectangle2D.Double(10,100,400,400)); //ÌîÂú¸ø¶¨µÄ¾ØĞÎ¾ÖÓò
+		    	g2.setPaint(myPaint);
+	    	    g2.fill(new Rectangle2D.Double(10,100,400,400)); //å¡«æ»¡ç»™å®šçš„çŸ©å½¢å±€åŸŸ
 		    }
 	    	 if(mapx[((int)a-10)/10][((int)b-100)/10]==1){        //sun.awt.RepaintArea.paintComponent(Unknown Source)
 					g2.setPaint(Color.black);
@@ -223,138 +178,7 @@ public class Qipant extends Frame{
 					g2.setPaint(Color.blue);
 					g2.fill(new Ellipse2D.Double(a,b,10,10));
 			    	}
-	}
-	
-	public boolean Bw(){
-		if(click%2==0) return true;
-		else return false;
-	}
-	public void output(double x,double y,int a,int b,boolean in){
-    	System.out.println("("+x+","+y+")"+"  ("+a+","+b+")"+"  ÊÇ·ñÔÚÆåÅÌÄÚ£º"+in);
-    	}
-    public void locate(double x,double y){
-        int temp1=(int)x; int temp2=(int)y;
-    	
-        a=temp1/10*10;
-        b=temp2/10*10;
-    	
-    }
- 
-    public void initializemapx(){
-    	for(int i=0;i<40;i++){
-    		for(int j=0;j<40;j++){
-    			mapx[i][j]=0;
-    		}
-    	}
-    }
-
-	void shuipingxy(int i,int j,int bw,int[][] mapx,boolean[][] map1){
-		if(mapx[i][j]==bw){
-			map1[i][j]=false;
-			num3++;
-			shuiping(i,j,bw);
-		}
-		if(mapx[i][j]!=bw) return;
-	}
-	void shuiping(int i,int j,int bw){
-		
-		if(i<40 && j+1<40 && i>=0 && j+1>=0 && map1[i][j+1]){shuipingxy(i,j+1,bw,mapx,map1);}  //Ô½½çÔò²»´´½¨¸±±¾
-		
-		if(i<40 && j-1<40 && i>=0 && j-1>=0 && map1[i][j-1]){shuipingxy(i,j-1,bw,mapx,map1);}  //shuipingxy(i,j-1) µ÷ÓÃÁË¸¸ÀàµÄº¯Êı   º¯ÊıÔÚmapÖ´ĞĞ   ¶ø²»ÊÇmap3
-		
-	}
-	void chuizhixy(int i,int j,int bw,int[][] mapx,boolean[][] map1){
-		if(mapx[i][j]==bw){
-			map1[i][j]=false;
-			num4++;
-			chuizhi(i,j,bw);
-		}
-		if(mapx[i][j]!=bw) return;
-	}
-	void chuizhi(int i,int j,int bw){
-		
-		if(i-1<40 && j<40 && i-1>=0 && j>=0 && map1[i-1][j]){chuizhixy(i-1,j,bw,mapx,map1);}  //Ô½½çÔò²»´´½¨¸±±¾
-		
-		if(i+1<40 && j<40 && i+1>=0 && j>=0 && map1[i+1][j]){chuizhixy(i+1,j,bw,mapx,map1);}
-		
-	}
-	void zhengxiexy(int i,int j,int bw,int[][] mapx,boolean[][] map1){
-		if(mapx[i][j]==bw){
-			map1[i][j]=false;
-			num2++;
-			zhengxie(i,j,bw);
-		}
-		if(mapx[i][j]!=bw) return;
-	}
-	void zhengxie(int i,int j,int bw){
-		
-		if(i-1<40 && j+1<40 && i-1>=0 && j+1>=0 && map1[i-1][j+1]){zhengxiexy(i-1,j+1,bw,mapx,map1);}  //Ô½½çÔò²»´´½¨¸±±¾
-	
-		if(i+1<40 && j-1<40 && i+1>=0 && j-1>=0 && map1[i+1][j-1]){zhengxiexy(i+1,j-1,bw,mapx,map1);}
-	
-	}
-	void fanxiexy(int i,int j,int bw,int[][] mapx,boolean[][] map1){
-		
-		if(mapx[i][j]!=bw) return;
-		if(mapx[i][j]==bw){
-			map1[i][j]=false;
-			num1++;
-			fanxie(i,j,bw);
-		}
-	}
-	void fanxie(int i,int j,int bw){
-
-		if(i+1<40 && j+1<40 && i+1>=0 && j+1>=0 && map1[i+1][j+1]){fanxiexy(i+1,j+1,bw,mapx,map1);}  //Ô½½çÔò²»´´½¨¸±±¾
-		
-		if(i-1<40 && j-1<40 && i-1>=0 && j-1>=0 && map1[i-1][j-1]){fanxiexy(i-1,j-1,bw,mapx,map1);}
-    }
-    boolean[][] initializeBoolean(int bw,int[][] mapx){
-    	for(int i=0;i<40;i++){
-    		for(int j=0;j<40;j++){       //Ç¶Ì×Ñ­»·£¬³õÊ¼»¯Boolean¶şÎ¬Êı×é£¬
-    			if(mapx[i][j]==bw) map1[i][j]=true;//±ÈÈçËµ´«ÈëµÄÆå×ÓÊÇºÚÆå£¬¼´bw=1;Ôò³õÊ¼»¯µÄ½á¹ûÊÇ£º
-    			else map1[i][j]=false; //     intÊı×éºÍBooleanÊı×éÆäÊµ½á¹¹Ò»ÑùµÄ£¬ÔÚbw=1µÄµØ·½¡£ÔÚ
-    			                       //     BooleanÊı×éÉÏ¸³Öµtrue.
-    		}
-    	}
-    	return map1;
-    }
-    public void listener(int num1,int num2,int num3,int num4,int count1,int count2,int count3,int count4){
-    	int [] array={num1,num2,num3,num4,count1,count2,count3,count4};
-    	System.out.println(num1+"  "+num2+"  "+num3+"  "+num4+"  "+count1+"  "+count2+"  "+count3+"  "+count4);
-    	for(int i=0;i<8;i++){
-    		
-    		if(array[i]==5){
-    			Frame f=new Frame("game over");
-            	Panel p=new Panel();
-        		p.setLayout(new FlowLayout());
-        		p.add(new Button("yes"));
-        		p.add(new Button("no"));
-        		
-    			if(i>=0 && i<=3){
-    				f.setBackground(Color.LIGHT_GRAY);
-    				f.add(new Label("the black site is the big ass.."),BorderLayout.CENTER);
-    			}
-    			if(i>=4 && i<=7){
-    				f.setBackground(Color.blue);
-    				f.add(new Label("the blue site is the boss!!"),BorderLayout.CENTER);
-    				}
-    			f.add(p,BorderLayout.SOUTH);
-    			f.setSize(300,150);
-            	f.setVisible(true);
-    			
-    			f.addWindowListener(new WindowAdapter(){
-    				public void windowClosing(WindowEvent e){
-    					System.out.println("windowclsing..");
-    					dispose();
-    				}
-    			});
-            	
-    		}
-    		
-    	}
-    	
-    }
-	
+	}	
 }
 
 
