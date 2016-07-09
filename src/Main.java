7d454dee745026891fac4d190800485813ce6bc5
butcherInc <843968535@qq.com>
2016-07-10 02:08:30 +0800

    Revert "github上的项目好像没有Main.java,actionProcessThread.java两个文件 需要加入重新推送。"
    This reverts commit 58f5733724b40a14fbb2700283efeacfc36cf43e.

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-    public static void main(String[] args){
-        System.out.println("main(): "+Thread.currentThread().getName());
-        Qipant qipant=new Qipant("Îå×ÓÆå");
-        qipant.setVisible(true);
-    }
-}
