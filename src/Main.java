c92d072062e1f48be560fd115c05c8e39ddb482a
butcherInc <843968535@qq.com>
2016-07-09 19:53:35 +0800

    小改动

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("main(): "+Thread.currentThread().getName());
+        System.out.println("this.main(): "+Thread.currentThread().getName());
         Qipant qipant=new Qipant("Îå×ÓÆå");
         qipant.setVisible(true);
     }
