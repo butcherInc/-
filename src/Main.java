58f5733724b40a14fbb2700283efeacfc36cf43e
butcherInc <843968535@qq.com>
2016-07-09 19:52:11 +0800

    github上的项目好像没有Main.java,actionProcessThread.java两个文件 需要加入重新推送。

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("main(): "+Thread.currentThread().getName());
+        Qipant qipant=new Qipant("Îå×ÓÆå");
+        qipant.setVisible(true);
+    }
+}
